6f867b2c655d48f29946e33d44b4e7b1b816f65c
anbingxu666 <anbingxu666@163.com>
2018-08-20 17:53:18 +0800

    2018/8/20 线性回归的学习-SimpleLinearRegression

--- my_knn_classifier.py
+++ my_knn_classifier.py
@@ -16,11 +16,12 @@ class my_knn_classifier:
         y_predict=[self._predict(x) for x in X_predict]
         return np.array(y_predict)
     def _predict(self,x):
+        '''功能：预测单个x的y'''
         x.reshape((1,-1))
         distance = np.sqrt(np.sum(((self._X_train-x)**2),axis=1))
-        res_index=np.argsort(distance)
-        first_k_item=[self._Y_train[the_k_item] for the_k_item in res_index[:self.k]]
-        predict_result=Counter(first_k_item).most_common(1)[0][0]
+        res_index=np.argsort(distance) # 顺序从小到大的下标
+        first_k_item=[self._Y_train[the_k_item] for the_k_item in res_index[:self.k]] #找到距离最大的前k个的y值
+        predict_result=Counter(first_k_item).most_common(1)[0][0] #选出出现频率最高的1个y作为结果
         return predict_result
     # def _predict(self, x):
     #     """给定单个待预测数据x，返回x的预测结果值"""
