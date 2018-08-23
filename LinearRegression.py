14e423e108f04a7db85f65a7f911845ced85a106
anbingxu666 <anbingxu666@163.com>
2018-08-23 23:03:15 +0800

    2018/8/23 Batch and Stochastic Gradient Desent手动实现

--- LinearRegression.py
+++ LinearRegression.py
@@ -15,13 +15,53 @@ class LinearRegression():
         self.cofficients_ = self._theta[1:]
         self.intercept_ = self._theta[:1]
         return self
+
+    def fit_gd(self, X_train, y_train, eta=0.01, n_iters=1e4):
+        """根据训练数据集X_train, y_train, 使用梯度下降法训练Linear Regression模型"""
+        assert X_train.shape[0] == y_train.shape[0], \
+            "the size of X_train must be equal to the size of y_train"
+
+        def J(theta, X_b, y):
+            try:
+                return np.sum((y - X_b.dot(theta)) ** 2) / len(y)
+            except:
+                return float('inf')
+
+        def dJ(theta, X_b, y):
+            return X_b.T.dot(X_b.dot(theta) - y) * 2. / len(y)
+
+        def gradient_descent(X_b, y, initial_theta, eta, n_iters=1e4, epsilon=1e-8):
+
+            theta = initial_theta
+            cur_iter = 0
+
+            while cur_iter < n_iters:
+                gradient = dJ(theta, X_b, y)
+                last_theta = theta
+                theta = theta - eta * gradient
+                if (abs(J(theta, X_b, y) - J(last_theta, X_b, y)) < epsilon):
+                    break
+
+                cur_iter += 1
+
+            return theta
+
+        X_b = np.hstack([np.ones((len(X_train), 1)), X_train])
+        initial_theta = np.zeros(X_b.shape[1])
+        self._theta = gradient_descent(X_b, y_train, initial_theta, eta, n_iters)
+
+        self.intercept_ = self._theta[0]
+        self.coef_ = self._theta[1:]
+
+        return self
+
     def predict(self,X):
         """给定X 预测y的值"""
         X = np.hstack([np.ones((X.shape[0],1)),X])
         predict_y = X.dot(self._theta)
         return predict_y
     def __repr__(self):
-        return "LinearRegression()  with normal_equation"
+        return "LinearRegression()"
 
     def score(self,X_test,y_test):
         """根据给定的测试数据集 计算R Square"""
